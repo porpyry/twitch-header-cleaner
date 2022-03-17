4999ed806baa8f6d3466e7e143604d4c50a9f517
porpyry <porpyry@gmail.com>
2022-03-17 18:54:36 +0900

    210317

--- content-script.js
+++ content-script.js
@@ -1,5 +1,5 @@
 (() => {
-  let handler = 0;
+  let handlers = {};
 
   function getElement(c) {
     return document.querySelector('div.' + c);
@@ -41,11 +41,9 @@
     // stream header
     const sh = getElement('stream-chat-header');
     if (sh) {
-      const h1 = getElement('channel-leaderboard');
-      const h2 = getElement('community-highlight');
-      const h3 = getElement(
-        'community-highlight-stack__backlog-card'
-      );
+      const h1 = getElement('channel-leaderboard'); // leaderboard (supporter)
+      const h2 = getElement('community-highlight'); // highlight (toto)
+      const h3 = getElement('community-highlight-stack__backlog-card'); // train
       if (
         !isShown(sh) &&
         !isShown(h1) &&
@@ -65,50 +63,32 @@
     }
   }
 
-  function clearChatHeader() {
-    // video header
-    const vh = getElement('video-chat__header');
-    if (vh) {
-      hide(vh);
-      return;
-    }
-
-    // stream header
-    const sh = getElement('stream-chat-header');
-    if (sh) {
-      const h1 = getElement('channel-leaderboard');
-      const h2 = getElement('community-highlight');
-      const h3 = getElement(
-        'community-highlight-stack__backlog-card'
-      );
-      hide(sh);
-      hide(h1);
-      hide(h2);
-      hide(h3);
-    }
-  }
-
   // check leaderboard for count n
-  function clearLeaderboard(n) {
+  function clearElement(n, name) {
     if (n > 0) {
-      handler = setTimeout(() => {
-        const h1 = getElement('channel-leaderboard');
-        if (h1) {
-          hide(h1);
-          handler = 0;
+      handlers[name] = setTimeout(() => {
+        const el = getElement(name);
+        if (el) {
+          hide(el);
+          handlers[name] = 0;
         } else {
-          clearLeaderboard(n - 1);
+          clearElement(n - 1, name);
         }
-      }, 100);
+      }, 500);
     } else {
-      handler = 0;
+      handlers[name] = 0;
     }
   }
 
   function clearHandler() {
-    if (handler) {
-      clearTimeout(handler);
-      handler = 0;
+    for (const name in handlers) {
+      if (Object.hasOwnProperty.call(handlers, name)) {
+        const handler = handlers[name];
+        if (handler) {
+          clearTimeout(handler);
+        }
+        handlers[name] = 0;
+      }
     }
   }
 
@@ -116,14 +96,21 @@
     (message, _sender, _sendResponse) => {
       if (message.event == 'click') {
         console.log('chat header toggle');
-        toggleChatHeader();
         clearHandler();
+        toggleChatHeader();
       } else if (message.event == 'load') {
         console.log('chat header clear');
-        clearChatHeader();
         clearHandler();
-        clearLeaderboard(150);
+        clearElement(30, 'video-chat__header'); // header (recent video)
+        clearElement(30, 'stream-chat-header'); // header (stream video)
+        clearElement(30, 'channel-leaderboard'); // leaderboard (supporter)
+        clearElement(30, 'community-highlight'); // highlight (toto)
+        clearElement(30, 'community-highlight-stack__backlog-card'); // train
       }
     }
   );
 })();
+
+// todo
+// 1. mutation observer -> check highlight, train
+// 2. on/off button
